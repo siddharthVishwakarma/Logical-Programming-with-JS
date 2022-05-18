6d1dfa80fa920a4fd455ae26f9065ca2272efbf2
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-05-18 14:46:00 +0530

    Reverse String✅

--- Reversed_Strings.js
+++ Reversed_Strings.js
@@ -1,5 +1,11 @@
 // Complete the solution so that it reverses the string passed into it.
 
-function solution(str) {
-  return str.split("").reverse().join("");
-}
+// function solution(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// const solution = (str) => str.split("").reverse().join("");
+
+const solution = (s) => [...s].reverse().join("");
+
+console.log(solution("hello"));
