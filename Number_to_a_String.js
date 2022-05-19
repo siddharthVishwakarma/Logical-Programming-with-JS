38664c4a3b4e8e4d50644f4d2c8de38933ba7e54
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-05-19 16:25:14 +0530

    num to str✅

---
+++ Number_to_a_String.js
@@ -0,0 +1,9 @@
+// Convert a Number to a String!
+
+// function numberToString(num) {
+//   return JSON.stringify(num);
+// }
+
+const numberToString = (num) => `${num}`;
+
+console.log(numberToString(333));
